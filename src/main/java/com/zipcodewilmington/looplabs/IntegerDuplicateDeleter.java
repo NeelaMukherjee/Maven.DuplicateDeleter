8dec14fb1c3066ed7449228125e7e4aafaafb85d
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-03-13 09:10:55 -0400

    used stream

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,47 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
-}
+
+    Integer[] intArray = super.array;
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        Stream<Integer> intStream = Arrays.stream(intArray).filter(integer -> counter(integer) < maxNumberOfDuplications);
+        Integer[] arrayWithoutDuplicates = intStream.toArray(Integer[]::new);
+        return arrayWithoutDuplicates;
+    }
+
+    public Integer counter(Integer integer) {
+        int counter = 0;
+        for (Integer num: intArray) {
+            if(num.equals(integer)) {
+                counter++;
+            }
+        } return counter;
+    }
+
+
+
+
+    @Override
+    public Integer[] removeDuplicatesExactly ( int exactNumberOfDuplications) {
+
+        Stream<Integer> intStream = Arrays.stream(intArray).filter(integer -> counter(integer) != exactNumberOfDuplications);
+        Integer[] arrayWithoutDuplicates = intStream.toArray(Integer[]::new);
+        return arrayWithoutDuplicates;
+    }
+    }
+
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,41 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    String [] stringArray = super.array;
+
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        Stream<String> stringStream = Arrays.stream(stringArray).filter(string -> counter(string) < maxNumberOfDuplications);
+        String[] arrayWithoutDuplicates = stringStream.toArray(String[]::new);
+        return arrayWithoutDuplicates;
+    }
+
+    public Integer counter(String string) {
+        int counter = 0;
+        for (String word: stringArray) {
+            if(word.equals(string)) {
+                counter++;
+            }
+        } return counter;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Stream<String> stringStream = Arrays.stream(stringArray).filter(string -> counter(string) != exactNumberOfDuplications);
+        String[] arrayWithoutDuplicates = stringStream.toArray(String[]::new);
+        return arrayWithoutDuplicates;
+    }
 }
